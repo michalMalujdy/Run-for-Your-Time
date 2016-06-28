6a71aaeb7c007728275e20215b70e20c297ffc77
Dexter <natri4x@gmail.com>
2016-06-28 20:29:58 +0200

    Removed chains collisions between themselves

---
+++ Assets/Scripts/Chain/ChainCellCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChainCellCollision : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "ChainCell")
+        {
+            Physics2D.IgnoreCollision(coll.collider, GetComponent<Collider2D>());
+        }
+    }
+}
